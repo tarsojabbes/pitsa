a183a73d6424847efddda4b41caf143decabbb89
sabrinabs <sabrina.silva@ufcg.edu.br>
2023-05-21 17:55:39 -0300

    adiciona código, já com testes, da US 10

---
+++ src/main/java/com/ufcg/psoft/mercadofacil/exception/SaborDisponivelException.java
@@ -0,0 +1,11 @@
+package com.ufcg.psoft.mercadofacil.exception;
+
+public class SaborDisponivelException extends MercadoFacilException{
+
+    public SaborDisponivelException(){
+
+        super("O sabor de pizza consultado esta disponivel.");
+
+    }
+
+}
