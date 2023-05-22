e3217cfff8b2dd8296656f6cce4be93f0cecd26d
Ívyna Rufino <80000710+ivynalavinia@users.noreply.github.com>
2023-05-22 13:19:26 -0300

    Revert "Revert "US11 + dois pontos da US8""

--- src/main/java/com/ufcg/psoft/mercadofacil/dto/PedidoPostPutRequestDTO.java
+++ src/main/java/com/ufcg/psoft/mercadofacil/dto/PedidoPostPutRequestDTO.java
@@ -1,22 +1,19 @@
 package com.ufcg.psoft.mercadofacil.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ufcg.psoft.mercadofacil.model.MeioDePagamento;
+import com.ufcg.psoft.mercadofacil.model.MeioDePagamentoValidator;
 import com.ufcg.psoft.mercadofacil.model.Pizza;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
-
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -30,23 +27,22 @@ public class PedidoPostPutRequestDTO {
     @NotEmpty(message = "A listagem de pedidos nao pode estar vazia.")
     private List<Pizza> pizzas;
 
-    @JsonProperty("codigoDeAcesso.")
+    @JsonProperty("codigoDeAcesso")
     @NotBlank(message = "Codigo de acesso nao pode estar em branco.")
     @NotNull(message = "Codigo de acesso nao pode ser null.")
     @NotEmpty(message = "Codigo de acesso nao pode ser vazio.")
-    @Size(min=6, message = "Codigo de acesso deve ter tamanho minimo de 6 digitos")
+    @Size(min = 6, message = "Codigo de acesso deve ter tamanho minimo de 6 digitos")
     private String codigoDeAcesso;
 
     @JsonProperty("meioDePagamento")
-    @NotBlank(message = "Meio de pagamento nao pode estar em branco.")
-    @NotEmpty(message = "Meio de pagamento nao pode ser vazio.")
-    @NotNull(message = "Meio de pagamento nao pode ser null.")
-    private String meioDePagamento;
+    @MeioDePagamentoValidator(regexp = "PIX|CREDITO|DEBITO")
+    @Enumerated(EnumType.STRING)
+    private MeioDePagamento meioDePagamento;
 
     @JsonProperty("idCliente")
     @NotNull(message = "A id do cliente nao deve ser nula.")
     @Positive(message = "A id do cliente deve ser maior que zero.")
-    private Long idCLiente;
+    private Long idCliente;
 
     @JsonProperty("enderecoAlternativo")
     private String enderecoAlternativo;
